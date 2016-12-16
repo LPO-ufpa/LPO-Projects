a0990bb27aaeb2092ac6134ad724b931a92d500d
Daniel Souza <souza_daniel13@hotmail.com>
2016-12-16 15:38:08 -0300

    Update Installation Cost

--- TCO_CRAN.m
+++ TCO_CRAN.m
@@ -1,7 +1,6 @@
 % TCO of C-RAN Architecture for 5G.
 
 % Steps
-%   Traffic Demand
 %   CAPEX
 %   OPEX
 
@@ -9,48 +8,15 @@ clear all;
 close all;
 clc;
 
-%% --- TRAFFIC DEMAND --- %%
-
-Area = 100                                    % Area in Km^2
-Population = 300000;                          % Total population of the area
-Sub_Fraction = 0.16;                          % Fraction of subscribers
-global User_Density;                          % Subscriber Density in the area
-global Mob_Sub;                               % Number of Mobile Subscribers
-N_oper = 1;                                   % Number of Mobile Operators
-Peak_TD = 0.16;                               % Traffic Demand at peak hours [16%]
-
-DR_total = 0   ;                              % Data rate per user * Number of users
-global Daily_Traffic;                         % Daily generated traffic [Mbps/km^2]
-
-Mob_Sub = Population * Sub_Fraction;
-User_Density = Mob_Sub / Area;
-%{
-H_userP;                                      % Heavy user Fraction
-O_userP = 100 - H_userP;                      % Ordinary user Fraction
-
-Hourly_DR_H = [HDR_H_pc HDR_H_tab HDR_H_pho]; % Hourly DR for heavy users
-Hourly_DR_O = [HDR_O_pc HDR_O_tab HDR_O_pho]; % Hourly DR for ordin users 
-
-Data_rate = [DR_pc DR_tab DR_pho];            % Average data rate 
-Subscribers = [Sub_pc Sub_tab Sub_pho];       % Fraction of subscribers 
-
-for i = 1:3                             
-    Data_rate(i) = (H_userP * Hourly_DR_H(i) + O_userP * Hourly_DR_O(i))/45000;
-    DR_total = DR_total + (Data_rate(i) * Subscribers(i));
-end
-
-
-Daily_Traffic = (User_Density * Peak_TD * DR_total) / N_oper;
-%}
 %% --- CAPEX --- %%
 
-%% - Equipment Cost
+%% - Equipment Cost 
 % Sum of all equipment times the price of each equipment
 
 global TOT_EQcost;
 
 P_RRH = 4472;                          % Price per RRH
-N_RRH = 20;                             % Quantity of RRHs
+N_RRH = 20;                            % Quantity of RRHs
 P_BBUpool = 3000;                      % Price per BBU Pool
 N_BBUpool = 5;                         % Quantity of BBU Pools
 TOT_EQcost = 0;                        % Total equipment cost
@@ -60,33 +26,66 @@ EQ_quant = [N_RRH N_BBUpool];
 
 
 for i = 1:length(EQ_price)
-        TOT_EQcost = TOT_EQcost + EQ_price(i)*EQ_quant(i);
+        TOT_EQcost = TOT_EQcost + EQ_quant(i)* EQ_price(i);
 end
 
 %% - Infrastructure Cost
 % Deployment of fiber infrastructure, cost of leasing fibers and installation of RRH's
 
-global TOT_PTPCost;
+global TOT_PTPInfraCost;
 global TOT_LeaseFiberCost;
 global TOT_InfraCost;
 
 TOT_TrenchDist = 10;                 % Total distance to be trenched
-FiberTrench_KM = 130000;             % Trenching price per Km
-FiberLenght_KM = 30;                 % Total lenght of fiber needed
+TrenchPrice = 130000;                % Trenching price per Km
+FiberLenght = 30;                    % Total lenght of fiber needed
 FiberPrice = 160;                    % Price of fiber per KM
 
 TOT_LeaseDist = 0;                   % Total lenght of leased fiber
 FiberLease_KM = 200;                 % Leasing price per KM
 
-TOT_PTPCost = TOT_TrenchDist * FiberTrench_KM + FiberLenght_KM * FiberPrice;
-TOT_LeaseFiberCost = TOT_LeaseDist * FiberLease_KM;
+TOT_PTPInfraCost = TOT_TrenchDist * TrenchPrice + FiberLenght * FiberPrice; %EQ(3.12)
 
-TOT_InfraCost = TOT_PTPCost + TOT_LeaseFiberCost;
+TOT_LeaseFiberCost = TOT_LeaseDist * FiberLease_KM; %EQ(3.20)
+
+TOT_InfraCost = TOT_PTPInfraCost + TOT_LeaseFiberCost; %EQ(3.11)
 
 %% - Installation Cost
 % Expenses related to installing the backhaul components in their locations
 
+global TOT_CostInstall;
+global TOT_FiberInstCost;
+global TOT_MWInstCost;
 
+%fiber
+T_InstPort_F= 20;                     % Total time to install a equipament port
+N_Port_F= 10;                         % Number of ports to be install
+N_Eq_F= 20;                           % Total number of equipment cost 
+Pr_Tech_F= 30;                        % Technician salary per hour
+
+for k = 1:lenght(T_InstPort_F * N_Port_F);
+TOT_FiberInstCost= TOT_FiberInstCost + (T_InstPort_F * N_Port_F) * (N_Eq_F * Pr_Tech_F);
+
+
+end
+
+%microwave
+T_InstPort_Mw= 20;                     % Total time to install a equipament port
+N_Port_Mw= 10;                         % Number of ports to be install
+N_Eq_Mw= 20;                           % Total number of equipment cost 
+Pr_Tech_Mw= 30;                        % Technician salary per hour
+
+for k = 1:lenght(T_InstPort_Mw * N_Port_Mw);
+TOT_MWInstCost= TOT_MWInstCost + (T_InstPort_Mw * N_Port_Mw) * (N_Eq_Mw * Pr_Tech_Mw);
+
+
+end
+
+ TOT_CostInstall= TOT_FiberInstCost + TOT_MWInstCost; %EQ(3.22)
+
+
+ 
+ 
 %% --- OPEX --- %%
 
 %% - Energy Cost
@@ -94,7 +93,7 @@ TOT_InfraCost = TOT_PTPCost + TOT_LeaseFiberCost;
 
 global Yearly_kW;                   % Yearly kW consumed by equipments
 global Price_kW;                    % Price per kW
-global TOT_EnergyCost;                % Total energy cost
+global TOT_EnergyCost;              % Total energy cost
 
 for i = 1:length(Yearly_kW);
     TOT_EnergyCost = TOT_EnergyCost + (Yearly_kW(i)*Price_kW);
